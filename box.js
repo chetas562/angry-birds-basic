c0fa172a325f54ce724a7e02ec64a3cba39d5236
chetas562 <70834532+chetas562@users.noreply.github.com>
2020-09-27 11:13:05 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,23 @@
+class Box {
+    constructor (x,y,width,height){
+       var options={
+        restitution:  1  
+       }  
+      this.body=Bodies.rectangle(x,y,width,height);
+         World.add(world,this.body );
+    } 
+    display(){
+     var pos =this.body.position;
+     var angle = this.body.angle;
+     push();
+     rotate(angle);
+     translate(pos.x,pos.y);
+     rectMode(CENTER);
+      fill("yellow");
+      strokeWeight(4);
+      stroke("Green");
+      rect(0,0,this.width,this.height);
+      pop(); 
+    }
+  }
+  
